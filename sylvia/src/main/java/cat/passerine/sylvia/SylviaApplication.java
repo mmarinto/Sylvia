28dc6c92bcc43850a1cc1ec1a50b8842e0b782fe
mmarin <mmarin@PR158>
2018-08-08 12:30:16 +0200

    minimum

---
+++ sylvia/src/main/java/cat/passerine/sylvia/SylviaApplication.java
@@ -0,0 +1,12 @@
+package cat.passerine.sylvia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SylviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SylviaApplication.class, args);
+	}
+}
