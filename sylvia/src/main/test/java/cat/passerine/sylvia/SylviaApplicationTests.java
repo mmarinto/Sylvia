4f92e8f6e09ef77d2ce4adae8d7daa99952bbdf8
mmarin <marta.registre@gmail.com>
2018-08-09 13:17:18 +0200

    Test file (Eclipse)

---
+++ sylvia/src/main/test/java/cat/passerine/sylvia/SylviaApplicationTests.java
@@ -0,0 +1,20 @@
+package cat.passerine.sylvia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+/**
+ * Test class
+ * @author mmarin
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SylviaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
